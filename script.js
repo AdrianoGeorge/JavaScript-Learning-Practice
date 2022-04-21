23b5d58e14dc84b86a3b53c8e0b1ed5fd6e42d89
Adrian George <adrian.george@wearewiser.com>
2022-04-21 11:52:19 +0100

    JavaScript Template Literals

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-//  --------------    JavaScript String Search --------------
+//  --------------   JavaScript Template Literals -------------
 
-// JavaScript String indexOf()
-// The indexOf() method returns the index of (the position of) the first occurrence of a specified text in a string:
-
-let str = "Please locate where 'locate' occurs!";
-str.indexOf("locate");
-
-// JavaScript String match()
-// The match() method searches a string for a match against a regular expression, and returns the matches, as an Array object.
-
-// Example 1
-// Search a string for "ain":
-
-let text1 = "The rain in SPAIN stays mainly in the plain";
-text.match(/ain/g); // Returns ["ain", "ain"]
-
-// JavaScript String includes()
-// The includes() method returns true if a string contains a specified value.
+// Back-Tics Syntax
+// Template Literals use back-ticks (``) rather than the quotes ("") to define a string:
 
 // Example
-let text2 = "Hello world, welcome to the universe.";
-text.includes("world"); // Returns true
+let text = `Hello World!`;
+
+// Automatic replacing of variables with real values is called string interpolation.
+let firstName = "John";
+let lastName = "Doe";
+
+let text2 = `Welcome ${firstName}, ${lastName}!`;
