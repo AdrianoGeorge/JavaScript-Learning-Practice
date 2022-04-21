98a7b0b0cc40eabf313760ee67e8190640398d1a
Adrian George <adrian.george@wearewiser.com>
2022-04-21 11:05:08 +0100

    Js let and CONST

--- script.js
+++ script.js
@@ -52,14 +52,14 @@ let m, g;
 m = 5;
 g = 6;
 
-// When to Use JavaScript const?
+//  --------------   When to Use JavaScript const   --------------  ?
 // If you want a general rule: always declare variables with const.
 
 // If you think the value of the variable can change, use let.
 
 // In this example, price1, price2, and total, are variables:
 
-Example;
+// Example;
 const price1 = 5;
 const price2 = 6;
 let total = price1 + price2;
@@ -72,7 +72,7 @@ let total = price1 + price2;
 
 // Variables defined with let have Block Scope.
 
-// Block Scope
+//  --------------  Block Scope  --------------
 // Before ES6 (2015), JavaScript had only Global Scope and Function Scope.
 
 // ES6 introduced two important new JavaScript keywords: let and const.
@@ -84,3 +84,46 @@ let total = price1 + price2;
   let x = 2;
 }
 // x can NOT be used here
+
+//  --------------  Redeclaring Variables --------------
+// Redeclaring a variable using the var keyword can impose problems.
+
+// Redeclaring a variable inside a block will also redeclare the variable outside the block:
+
+// Example
+var x = 10;
+// Here x is 10
+
+{
+  var x = 2;
+  // Here x is 2
+}
+
+// Here x is 2
+
+// Redeclaring a variable using the let keyword can solve this problem.
+
+// Redeclaring a variable inside a block will not redeclare the variable outside the block:
+
+// --------------   Let Hoisting  -------------- //
+// Variables defined with var are hoisted to the top and can be initialized at any time.
+
+// Meaning: You can use the variable before it is declared:
+
+// This is OK:
+
+carName = "Volvo";
+var carName;
+
+// Variables defined with let are also hoisted to the top of the block, but not initialized.
+
+// Meaning: Using a let variable before it is declared will result in a ReferenceError:
+
+//   --------------  JavaScript Constants  --------------
+// The const keyword was introduced in ES6 (2015).
+
+// Variables defined with const cannot be Redeclared.
+
+// Variables defined with const cannot be Reassigned.
+
+// Variables defined with const have Block Scope.
