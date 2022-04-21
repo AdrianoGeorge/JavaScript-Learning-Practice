f1c021607516d1301965778cccd8f2ad58fddc22
Adrian George <adrian.george@wearewiser.com>
2022-04-21 11:31:49 +0100

    JavaScript Objects

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//  --------------   Local Variables  --------------
-// Variables declared within a JavaScript function, become LOCAL to the function.
+//  --------------    JavaScript Objects --------------
+// What is this?
+// In JavaScript, the this keyword refers to an object.
 
-// Local variables can only be accessed from within the function.
+// Which object depends on how this is being invoked (used or called).
 
-Example;
-// code here can NOT use carName
+// The this keyword refers to different objects depending on how it is used:
 
-function myFunction() {
-  let carName = "Volvo";
-  // code here CAN use carName
-}
-
-// code here can NOT use carName
+// In an object method, this refers to the object.
+// Alone, this refers to the global object.
+// In a function, this refers to the global object.
+// In a function, in strict mode, this is undefined.
+// In an event, this refers to the element that received the event.
+// Methods like call(), apply(), and bind() can refer this to any object.
