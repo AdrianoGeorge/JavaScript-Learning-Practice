fc6445da6bfb6f79707e52f71ab76feb6d361850
Adrian George <adrian.george@wearewiser.com>
2022-04-21 11:49:54 +0100

    JavaScript String Search

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-//  --------------    JavaScript String Methods --------------
+//  --------------    JavaScript String Search --------------
 
-// Method	Description
-// charAt()	Returns the character at a specified index (position)
-// charCodeAt()	Returns the Unicode of the character at a specified index
-// concat()	Returns two or more joined strings
-// endsWith()	Returns if a string ends with a specified value
-// fromCharCode()	Returns Unicode values as characters
-// includes()	Returns if a string contains a specified value
-// indexOf()	Returns the index (position) of the first occurrence of a value in a string
-// lastIndexOf()	Returns the index (position) of the last occurrence of a value in a string
-// localeCompare()	Compares two strings in the current locale
-// match()	Searches a string for a value, or a regular expression, and returns the matches
-// repeat()	Returns a new string with a number of copies of a string
-// replace()	Searches a string for a value, or a regular expression, and returns a string where the values are replaced
-// search()	Searches a string for a value, or regular expression, and returns the index (position) of the match
-// slice()	Extracts a part of a string and returns a new string
-// split()	Splits a string into an array of substrings
-// startsWith()	Checks whether a string begins with specified characters
-// substr()	Extracts a number of characters from a string, from a start index (position)
-// substring()	Extracts characters from a string, between two specified indices (positions)
-// toLocaleLowerCase()	Returns a string converted to lowercase letters, using the host's locale
-// toLocaleUpperCase()	Returns a string converted to uppercase letters, using the host's locale
-// toLowerCase()	Returns a string converted to lowercase letters
-// toString()	Returns a string or a string object as a string
-// toUpperCase()	Returns a string converted to uppercase letters
-// trim()	Returns a string with removed whitespaces
-// valueOf()	Returns the primitive value of a string or a string object
+// JavaScript String indexOf()
+// The indexOf() method returns the index of (the position of) the first occurrence of a specified text in a string:
+
+let str = "Please locate where 'locate' occurs!";
+str.indexOf("locate");
+
+// JavaScript String match()
+// The match() method searches a string for a match against a regular expression, and returns the matches, as an Array object.
+
+// Example 1
+// Search a string for "ain":
+
+let text1 = "The rain in SPAIN stays mainly in the plain";
+text.match(/ain/g); // Returns ["ain", "ain"]
+
+// JavaScript String includes()
+// The includes() method returns true if a string contains a specified value.
+
+// Example
+let text2 = "Hello world, welcome to the universe.";
+text.includes("world"); // Returns true
